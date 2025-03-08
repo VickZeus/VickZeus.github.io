4abbc3924088ef2e90c4f6e45d30423e6aa3078d
LOSTSOUL <abhisheksingh160405@gmail.com>
2025-03-08 13:09:09 +0530

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,17 +1,19 @@
 const arr=["home","about","contactinfo","projects"];
-function showsection(id)
+const button=["but_home","but_about","but_contact","but_project","but_resume"];
+
+function showsection(id,id2)
 {
-    //document.getElementById(prev).style.display="none";
     for(let i=0;i<arr.length;i++)
     {
         if(arr[i]==id)
         {
             document.getElementById(arr[i]).style.display="block";
-           // prev=id;
+            document.getElementById(id2).style.color="rgb(158, 157, 157)";
         }
         else 
         {
             document.getElementById(arr[i]).style.display="none";
+           document.getElementById(button[i]).style.color="rgb(0, 0, 0)";
         }
     }
 }
